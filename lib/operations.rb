79f3017eac090718a8eb69a1fcb74d6e17254b04
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-24 18:37:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,16 @@
 def unsafe?(speed)
   if speed = speed<40 || speed>60
-    
+    return "whoa there!"
   else
-    
+    "move along"
   end
 end
 
 
 
 def not_safe?(speed)
-  speed<40 || speed>60 ? 
+  speed<40 || speed>60 ? "whoa there!" : "move along"
   
-	end
 end
 	
 
